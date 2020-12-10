9a3d8e5e24e878b66c18f0936cb8868777c5bf31
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2020-12-10 15:32:30 +0530

    Update utils.py

--- ultroid/utils.py
+++ ultroid/utils.py
@@ -4,6 +4,7 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
+    from ultroid import bot
     path = Path(f"ultroid/plugins/{plugin_name}.py")
     name = "ultroid.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
@@ -12,4 +13,4 @@ def load_plugins(plugin_name):
     spec.loader.exec_module(load)
     sys.modules["ultroid.plugins." + plugin_name] = load
     print("Ultroid has Imported " + plugin_name)
-    mod.ultroid = ultroid
+    mod.bot = ultroid
