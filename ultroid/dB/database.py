28b9b0aec0a95d5ee39deac2f761c36d4d2bdecd
Aditya <58950863+xditya@users.noreply.github.com>
2020-12-10 15:45:19 +0530

    Update database.py

--- ultroid/dB/database.py
+++ ultroid/dB/database.py
@@ -6,6 +6,6 @@ ENV = bool(os.environ.get("ENV", False))
 
 class Var(object):
     API_ID = int(os.environ.get("API_ID", 9))
-    # 9 is a placeholder
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     STRING_SESSION = os.environ.get("STRING_SESSION", None)
