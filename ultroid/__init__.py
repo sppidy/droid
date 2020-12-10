5bde6b14898c4e9aca37effa75bfa19650a2d125
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2020-12-10 15:24:11 +0530

    Add files via upload

---
+++ ultroid/__init__.py
@@ -0,0 +1,46 @@
+import os, sys
+from telethon.sessions import StringSession
+from telethon import TelegramClient
+from var import Var
+os.system("pip install --upgrade pip")
+
+if Var.STRING_SESSION:
+    bot = TelegramClient(StringSession(Var.STRING_SESSION), Var.APP_ID, Var.API_HASH)
+else:
+    quit(1)
+
+ENV = os.environ.get("ENV", False)
+
+from logging import basicConfig, getLogger, INFO, DEBUG
+from distutils.util import strtobool as sb
+import asyncio
+
+from requests import get
+if bool(ENV):
+    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+    if CONSOLE_LOGGER_VERBOSE:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=DEBUG,
+        )
+    else:
+        basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    level=INFO)
+    LOGS = getLogger(__name__)
+    CONFIG_CHECK = os.environ.get(
+        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+
+    if CONFIG_CHECK:
+        LOGS.info(
+            "Please remove the line mentioned in the first hashtag from the config.env file"
+        )
+        quit(1)
+else:
+    PLACEHOLDER = None
+
+#
+
+
+
+# END OF VARS
