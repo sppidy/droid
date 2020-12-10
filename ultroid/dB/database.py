875097d1ee21a06a58b61eb4526bb5d1aa981adf
Aditya <58950863+xditya@users.noreply.github.com>
2020-12-10 16:32:15 +0530

    Update database.py

--- ultroid/dB/database.py
+++ ultroid/dB/database.py
@@ -8,4 +8,4 @@ class Var(object):
     API_ID = int(os.environ.get("API_ID", 9))
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    SESSION = os.environ.get("STRING_SESSION", None)
