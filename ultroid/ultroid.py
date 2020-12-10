7025b60fdd31d9238ad44ce973c896cbe19e85d8
Aditya <58950863+xditya@users.noreply.github.com>
2020-12-10 16:29:05 +0530

    Update ultroid.py

--- ultroid/ultroid.py
+++ ultroid/ultroid.py
@@ -33,6 +33,3 @@ for name in files:
         load_plugins(plugin_name.replace(".py", ""))
     
 print("Ultroid has been deployed! Visit @TheUltroid for updates!!")
-
-if __name__ == "__main__":
-    ultroid.run_until_disconnected()
