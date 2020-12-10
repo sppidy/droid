c6d9bf40b439d5d224e1029644aa70b68ab5af95
Aditya <58950863+xditya@users.noreply.github.com>
2020-12-10 18:40:58 +0530

    Update database.py

--- ultroid/dB/database.py
+++ ultroid/dB/database.py
@@ -9,3 +9,4 @@ class Var(object):
     API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     SESSION = os.environ.get("STRING_SESSION", None)
+    DB_URI = os.environ.get("DATABASE_URL", None)
