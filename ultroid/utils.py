e366524c3f8ff4ea45b0233c85f9a62ed7d84e69
Aditya <58950863+xditya@users.noreply.github.com>
2020-12-11 06:56:47 +0530

    Update utils.py

--- ultroid/utils.py
+++ ultroid/utils.py
@@ -3,14 +3,23 @@ import logging
 import importlib
 from pathlib import Path
 
-def load_plugins(plugin_name):
-    from ultroid import ultroid
-    path = Path(f"ultroid/plugins/{plugin_name}.py")
-    name = "ultroid.plugins.{}".format(plugin_name)
-    spec = importlib.util.spec_from_file_location(name, path)
-    load = importlib.util.module_from_spec(spec)
-    load.logger = logging.getLogger(plugin_name)
-    spec.loader.exec_module(load)
-    sys.modules["ultroid.plugins." + plugin_name] = load
-    print("Ultroid has Imported " + plugin_name)
-    mod.ultroid = ultroid
+def load_module(plugin_name):
+    if plugin_name.startswith("__"):
+        pass
+    elif plugin_name.endswith("_"):
+        import ultroid.utils
+        import importlib
+        path = Path(f"ultroid/plugins/{plugin_name}.py")
+        name = "ultroid.plugins.{}".format(plugin_name)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mod)
+        print("Ultroid has (re)Imported " + plugin_name)
+    else:
+        import ultroid.utils
+        import importlib
+        path = Path(f"ultroid/plugins/{plugin_name}.py")
+        name = "ultroid.plugins.{}".format(plugin_name)
+        spec = importlib.util.spec_from_file_location(name, path)
+        mod = importlib.util.module_from_spec(spec)
+        mod.ultroid = ultroid
